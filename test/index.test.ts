627e08df15de067bf28c89714fc7679076eb91cc
Ujjwal Sharma <usharma1998@gmail.com>
2019-05-31 23:58:51 +0530

    Add tests for third party modules

--- test/index.test.ts
+++ test/index.test.ts
@@ -12,12 +12,18 @@ test('secureRequire should work with core modules', () => {
   expect(strictFs).toEqual(fs);
 });
 
+test('secureRequire should work with third-party modules', () => {
+  const strictAcorn = secureRequire('acorn');
+  const acorn = require('acorn');
+  expect(strictAcorn).toEqual(acorn);
+});
+
 test('normal require compiles modules in the same context', () => {
   require('./fixtures/a');
   expect(require('./fixtures/b')).toBe(0);
-})
+});
 
 test('secureRequire compiles modules in different contexts', () => {
   secureRequire('../test/fixtures/a');
   expect(secureRequire('../test/fixtures/b')).toBe(1);
-})
+});
