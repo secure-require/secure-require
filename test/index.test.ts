ed9b2b9f9a5911d234f2cbe0d8b847ee5b9eea4a
Ujjwal Sharma <usharma1998@gmail.com>
2019-05-31 18:32:27 +0530

    Fix secureRequire test

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,5 +3,5 @@ import secureRequire from '../lib';
 test('secureRequire should work', () => {
   const strictFs = secureRequire('../jest.config.js');
   const fs = require('../jest.config.js');
-  expect(strictFs).toBe(fs);
+  expect(strictFs).toEqual(fs);
 });
