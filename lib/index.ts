d87663fdac8931c47b92152a1f0c91b4d576b1ed
Ujjwal Sharma <usharma1998@gmail.com>
2019-05-31 23:59:04 +0530

    Change code to handle third party modules

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-const module = require('module'); // Reasons:tm:
+const mod = require('module'); // Reasons:tm:
 import * as path from 'path';
 import * as vm from 'vm';
 
@@ -7,20 +7,23 @@ export default function secureRequire(
   this: any,
   specifier: string,
   context?: vm.Context
-): Object {
+): Object | undefined {
   if (!specifier || specifier === '') throw new Error();
   if (!context || !vm.isContext(context)) {
     context = vm.createContext();
   }
 
+  console.log(specifier);
+
   // If a NativeModule is required, not much can be done.
   // TODO: Talk to people about exposing the NativeModule class so that these
   // could be handled.
-  if (module.builtinModules.indexOf(specifier) > -1) {
+  if (mod.builtinModules.indexOf(specifier) > -1) {
     return require(specifier);
   }
 
-  const filename = module.Module._resolveFilename(specifier, this, false);
+  const filename = mod.Module._resolveFilename(specifier, module, false);
+  const newModule = new mod.Module(filename, module);
   const dirname = path.dirname(filename);
   const src = fs.readFileSync(filename, 'utf8');
   const fn = vm.compileFunction(
@@ -29,11 +32,14 @@ export default function secureRequire(
     { filename, parsingContext: context }
   );
 
-  // TODO: Make this simpler
-  // TODO: Once this is simpler, upstream that change to core
-  const exp = this.exports;
-  const thisValue = exp;
-  const mod = this;
-  fn.call(thisValue, exp, secureRequire, mod, filename, dirname);
-  return this.exports;
+  // const newModule = { exports: undefined, paths: module.paths };
+  fn.call(
+    newModule.exports,
+    newModule.exports,
+    secureRequire,
+    newModule,
+    filename,
+    dirname
+  );
+  return newModule.exports;
 }
