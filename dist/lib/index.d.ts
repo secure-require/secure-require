16751072a50baf23c483f280096e49eb6553217d
Ujjwal Sharma <usharma1998@gmail.com>
2019-06-01 01:12:11 +0530

    Remove files previously added from dist

--- dist/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-import * as vm from 'vm';
-export default function secureRequire(this: any, specifier: string, permittedModules?: Array<string>, context?: vm.Context): Object | undefined;
