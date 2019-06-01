4723cf57ff24d88c4898e2f361637f5f93547589
Ujjwal Sharma <usharma1998@gmail.com>
2019-06-01 16:46:51 +0530

    Attempt to fix caching

--- lib/index.ts
+++ lib/index.ts
@@ -3,18 +3,18 @@ const mod = require('module'); // Reasons:tm:
 import * as path from 'path';
 import * as vm from 'vm';
 
-const cache = Object.create(null);
-
 export default function secureRequire(
   this: any,
   specifier: string,
   permittedModules?: string[],
-  context?: vm.Context
+  context?: vm.Context,
+  cache?: { [index: string]: any }
 ): Object | undefined {
   if (!specifier || specifier === '') throw new Error();
   if (!context || !vm.isContext(context)) context = vm.createContext();
   if (!permittedModules || !Array.isArray(permittedModules))
     permittedModules = mod.builtinModules;
+  cache = cache || Object.create(null);
 
   // If a NativeModule is required, not much can be done.
   // TODO: Talk to people about exposing the NativeModule class so that these
@@ -26,16 +26,24 @@ export default function secureRequire(
   // TODO: Check if this resolves perfectly or if it should resolve relative to
   // the parent.
   // module = module.parent
-  const filename = mod.Module._resolveFilename(specifier, module, false);
+  const filename: string = mod.Module._resolveFilename(
+    specifier,
+    module,
+    false
+  );
+  const cached = cache![filename];
+  if (cached) {
+    return cached.exports;
+  }
+
   const newModule = new mod.Module(filename, module);
-  const cacheKey = filename + permittedModules!.join('.');
-  cache[cacheKey] = newModule;
+  cache![filename] = newModule;
   let threw = true;
   try {
-    secureLoad(newModule, filename, context, permittedModules!);
+    secureLoad(newModule, filename, context, permittedModules!, cache!);
     threw = false;
   } finally {
-    if (threw) delete cache[cacheKey];
+    if (threw) delete cache![filename];
   }
   return newModule.exports;
 }
@@ -44,7 +52,8 @@ function secureLoad(
   newModule: any,
   filename: string,
   context: vm.Context,
-  permittedModules: string[]
+  permittedModules: string[],
+  cache: Object
 ) {
   const dirname = path.dirname(filename);
   const src = fs.readFileSync(filename, 'utf8');
@@ -56,7 +65,7 @@ function secureLoad(
   compiled.call(
     newModule.exports,
     newModule.exports,
-    (id: any) => secureRequire(id, permittedModules, context),
+    (id: any) => secureRequire(id, permittedModules, context, cache),
     newModule,
     filename,
     dirname
